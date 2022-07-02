fbe3d4d496a7a917578ec2473f6558ec9568fe8d
AlanDeleon88 <19deleon88@gmail.com>
2022-07-02 14:04:07 -0700

    added new route that adds XSRF token to the req.csrftoken

--- authenticate-me/backend/routes/index.js
+++ authenticate-me/backend/routes/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router = express.Router();
+//test route
+// router.get('/hello/world' , (req, res) =>{
+//     res.cookie('XSRF-TOKEN', req.csrfToken());
+//     res.send('Hello World!');
+// });
 
-router.get('/hello/world' , (req, res) =>{
-    res.cookie('XSRF-TOKEN', req.csrfToken());
-    res.send('Hello World!');
+//add a XSRF-TOKEN cookie
+router.get('/api/csrf/restore', (req, res) =>{
+    const csrfToken = req.csrfToken();
+    res.cookie("XSRF-TOKEN", csrfToken);
+    res.status(200).json({
+        'XSRF-TOKEN' : csrfToken
+    });
 });
 
 module.exports = router;
