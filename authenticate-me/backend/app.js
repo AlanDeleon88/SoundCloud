4aa0c3edfe9f51ffba9959b12b82e6afdcdbf41c
AlanDeleon88 <19deleon88@gmail.com>
2022-07-02 13:18:57 -0700

    set up app.js

---
+++ authenticate-me/backend/app.js
@@ -0,0 +1,45 @@
+//import packages
+const express = require('express');
+require('express-async-errors');
+const morgan = require('morgan');
+const cors = require('cors');
+const csurf = require('csurf');
+const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
+
+//set environment and check production
+const { environment } = require('./config');
+const isProduction = environment === 'production';
+
+//init express
+const app  = express();
+//init morgan middleware
+app.use(morgan('dev'));
+//init cookieParser middleware
+app.use(cookieParser());
+//init express middleware for json body parsing.
+app.use(express.json());
+
+//Security middleware
+if(!isProduction) {
+    app.use(cors());
+}
+//init helmet security
+app.use(
+    helmet.crossOriginResourcePolicy({
+        policy : 'cross-origin'
+    })
+);
+
+//set csrf token and create req.csrf method
+app.use(
+    csurf({
+        cookie : {
+            secure : isProduction,
+            sameSite : isProduction && 'Lax',
+            httpOnly : true
+        }
+    })
+);
+
+
