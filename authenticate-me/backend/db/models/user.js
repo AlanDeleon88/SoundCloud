43161f43b708c93e9580c64aa8a89f8fb327fdae
AlanDeleon88 <19deleon88@gmail.com>
2022-07-09 15:51:44 -0700

    added methods to the user model for signing in, getting user, signing a new user up, toSafeObject to get user info to save in a jwt

--- authenticate-me/backend/db/models/user.js
+++ authenticate-me/backend/db/models/user.js
@@ -1,8 +1,8 @@
 'use strict';
-const {
-  Model,
-  Validator
-} = require('sequelize');
+const {Model, Validator } = require('sequelize');
+
+
+const bcrypt = require('bcryptjs');
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -10,8 +10,44 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
+     static associate(models) {
       // define association here
+    }
+    toSafeObject() {
+      const {id, username, email} = this; //!context of User instance.
+      return {id, username, email };
+    }
+
+    validatePassword(password) {
+      return bcrypt.compareSync(password, this.hashedPassword.toString());
+    }
+
+    static getCurrentUserById(id){
+      return User.scope('currentUser').findByPk(id);
+    }
+
+    static async login({credential, password}){
+      const {Op} = require('sequelize');
+      const user = await User.scope('loginUser').findOne({
+        where : {
+          [Op.or] : {
+            username : credential,
+            email: credential
+          }
+        }
+      });
+      if(user && user.validatePassword(password)){
+        return await User.scope('currentUser').findByPk(user.id);
+      }
+    }
+    static async signup({username, email, password}){
+      const hashedPassword = bcrypt.hashSync(password);
+      const user = await User.create({
+        username,
+        email,
+        hashedPassword
+      });
+      return await User.scope('currentUser').findByPk(user.id);
     }
   }
   User.init({
