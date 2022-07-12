8322a07c5650415055e50986d941a6d799716d1e
AlanDeleon88 <19deleon88@gmail.com>
2022-07-11 21:26:21 -0700

    added users.js router file and connected both session routes and user route to the api index

--- authenticate-me/backend/routes/api/index.js
+++ authenticate-me/backend/routes/api/index.js
@@ -6,6 +6,15 @@ const { User } = require('../../db/models');
 const { restoreUser } = require('../../utils/auth.js');
 const { requireAuth } = require('../../utils/auth.js');
 
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
+const { route } = require('./session.js');
+
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+
+
 router.post('/test', (req, res) =>{
     res.json({ requestBody : req.body });
 });
