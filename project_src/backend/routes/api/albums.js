d0f03a4438c731ca262e90a8bf1ac7681eedded7
AlanDeleon88 <19deleon88@gmail.com>
2022-07-16 14:44:40 -0700

    connected album router to api router, added endpoint in albums route to return all albums

--- project_src/backend/routes/api/albums.js
+++ project_src/backend/routes/api/albums.js
@@ -2,8 +2,25 @@ const express = require('express');
 
 const router = express.Router();
 
-const { setTokenCookie, requireAuth, restoreUser } = require('../../utils/auth');
+const { requireAuth, restoreUser } = require('../../utils/auth');
 const { User, Album } = require('../../db/models');
 
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
+
+
+router.get(
+    '/',
+    async (req, res, next) =>{
+        const albums = await Album.findAll();
+        console.log('whatt');
+        if(!albums){
+            let err = new Error('no albums could be found');
+            return next(err);
+        }
+        res.status = 200;
+        res.json({"Albums" : albums});
+    }
+)
+
+module.exports = router;
