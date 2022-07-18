3f95966c4ea5c11cad98d0b076816062c14eeaf0
AlanDeleon88 <19deleon88@gmail.com>
2022-07-17 20:25:28 -0700

    added a route to create a new album as current user

--- project_src/backend/routes/api/albums.js
+++ project_src/backend/routes/api/albums.js
@@ -8,8 +8,26 @@ const { User, Album } = require('../../db/models');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 
+const validateAlbum = [
+    check('title')
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage('Please provide a title'),
+    handleValidationErrors
+]
 
-router.get(
+// const validateLogin = [
+//     check('credential')
+//     .exists({ checkFalsy: true })
+//     .notEmpty()
+//     .withMessage('Please provide a valid email or username'),
+//     check('password')
+//     .exists({ checkFalsy: true })
+//     .withMessage('Please provide a password'),
+//     handleValidationErrors
+// ];
+
+router.get( //? get all albums endpoint
     '/',
     async (req, res, next) =>{
         const albums = await Album.findAll();
@@ -23,7 +41,7 @@ router.get(
     }
 )
 
-router.get(
+router.get( //? get albums by id endpoint
     '/:id',
     async (req, res, next) =>{
         let albumId = req.params.id;
@@ -55,15 +73,41 @@ router.get(
     }
 )
 
-router.use((err, _req, res, _next) => {
-    res.status(err.status || 500);
-    console.error(err);
-    res.json({
+router.post(
+    '/', //post a new album
+    [validateAlbum, requireAuth], //!validate req body
+   //! check if there is a session user
+    async (req, res, next) =>{
+        const {title, description, previewImage} = req.body;
+        console.log('test', title);
+        const { user } = req;
+        const  userId = req.user.id;
+        console.log(userId);
+        let newAlbum = await Album.create(
+            {
+                userId : userId,
+                title,
+                description: description || 'N/A',
+                previewImage : previewImage || 'N/A'
+            }
+        )
+        user.addAlbums([newAlbum]);
+        res.status = 201;
+        res.json(newAlbum);
 
-        message : err.message,
-        statusCode : err.status
+    }
+)
 
-        // stack : isProduction ? null : err.stack
-    });
-});
+// router.use((err, _req, res, _next) => { //! intercepts the regular error handler for validationError Handler
+
+//     res.status(err.status || 500);
+//     console.error(err);
+//     res.json({
+
+//         message : err.message,
+//         statusCode : err.status
+
+//         // stack : isProduction ? null : err.stack
+//     });
+// });
 module.exports = router;
