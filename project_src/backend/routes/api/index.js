f45f826eab6acfacaa0ddd33a1f0b99f23d474b8
AlanDeleon88 <19deleon88@gmail.com>
2022-07-19 21:58:29 -0700

    added foundations for songs router and connected to the index of api routes

--- project_src/backend/routes/api/index.js
+++ project_src/backend/routes/api/index.js
@@ -9,13 +9,16 @@ const { requireAuth } = require('../../utils/auth.js');
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 const albumsRouter = require('./albums.js');
+const songsRouter = require('./songs.js');
 
 
 router.use('/session', sessionRouter);
 
 router.use('/users', usersRouter);
 
-router.use('/albums', albumsRouter)
+router.use('/albums', albumsRouter);
+
+router.use('/songs', songsRouter);
 
 router.post('/test', (req, res) =>{
     res.json({ requestBody : req.body });
