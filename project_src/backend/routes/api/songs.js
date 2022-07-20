6e2e800f9673aa480bc015ce2b0f88355bdf948c
AlanDeleon88 <19deleon88@gmail.com>
2022-07-20 02:24:23 -0700

    added endpoint for deleting a song

--- project_src/backend/routes/api/songs.js
+++ project_src/backend/routes/api/songs.js
@@ -62,4 +62,39 @@ router.get(
     }
 );
 
+router.delete(
+    '/:id',
+    requireAuth,
+    async (req, res, next) =>{
+        const { id } = req.params;
+        const { user } = req;
+
+        const song = await Song.findByPk(id);
+
+        if(!song){
+            const err = new Error("Song couldn't be found"); //! maybe create a method to build these error handlers
+            err.title = 'Song not Found';
+            err.status = 404;
+            return next(err);
+        }
+        if(user.id !== song.userId){
+            const err = new Error("Song does not belong to current user"); //! maybe create a method to build these error handlers
+            err.title = 'Unauthorized delete';
+            err.status = 401;
+            return next(err);
+        }
+
+        await Song.destroy({
+            where:{
+                id: id
+            }
+        });
+        res.statusCode = 200
+        res.json({
+            "message" : "Successfully deleted",
+            "statusCode" : res.statusCode
+        })
+    }
+);
+
 module.exports = router;
