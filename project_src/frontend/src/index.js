b71e50c779edba16f0dec6d2a399199ae65016e7
AlanDeleon88 <19deleon88@gmail.com>
2022-08-27 00:38:23 -0700

    added code for backend to serve csrf token to frontend, added code for csrf store to store and recieve csrf token

--- project_src/frontend/src/index.js
+++ project_src/frontend/src/index.js
@@ -8,9 +8,14 @@ import { BrowserRouter } from 'react-router-dom';
 
 import configureStore from './store';
 
+import { restoreCSRF, csrfFetch } from './store/csrf';
+
 const store = configureStore();
 
 if(process.env.NODE_ENV !== 'production') {
+  restoreCSRF();
+  
+  window.csrfFetch = csrfFetch;
   window.store = store;
 }
 
---
+++ project_src/frontend/src/store/csrf.js
@@ -0,0 +1,29 @@
+import Cookies from "js-cookie";
+
+
+export const csrfFetch = async (url, options = {}) => {
+    //? set options.method to 'GET' if no method is passed in
+    options.method = options.method || 'GET';
+
+    //? set options.headers to an empty pojo if no headers.
+    options.headers = options.headers || {};
+
+    //? if the options.method is not 'GET, then set the 'Content-Type' header to
+    //? 'application/json', and set the 'XSRF-TOKEN' header to the value of the
+    //? 'XSRF-TOKEN' cookie
+    if(options.method.toUpperCase() !== 'GET'){
+        options.headers['Content-Type'] =
+            options.headers['Content-Type'] || 'application/json';
+        options.headers['XSRF-Token'] = Cookies.get('XSRF-TOKEN');
+    }
+
+    const res = await window.fetch(url, options);
+
+    if(res.status >= 400) throw res;
+
+    return res;
+}
+
+export const restoreCSRF = () => {
+    return csrfFetch('/api/csrf/restore');
+}
