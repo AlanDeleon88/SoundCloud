36fba10b28d6f4d4032f0b5d961d13c06f686dcf
AlanDeleon88 <19deleon88@gmail.com>
2022-08-27 15:02:32 -0700

    added a login navlink to homepage

--- project_src/frontend/src/App.js
+++ project_src/frontend/src/App.js
@@ -1,10 +1,11 @@
 import LogInFormPage from "./components/LoginFormPage";
-import { Route, Switch} from 'react-router-dom';
+import { Route, Switch, NavLink} from 'react-router-dom';
 
 function App() {
   return (
     <>
       <h1>Hello from App</h1>
+      <NavLink to='/login'>Login</NavLink>
       <Switch>
         <Route path='/login'>
           <LogInFormPage />
