44e02481ae947ad2a239358d8354b1187778927d
AlanDeleon88 <19deleon88@gmail.com>
2022-09-01 20:50:11 -0700

    added place holder songs to list and styles for chevrons

--- project_src/frontend/src/components/AlbumDetail/AlbumDetail.js
+++ project_src/frontend/src/components/AlbumDetail/AlbumDetail.js
@@ -1,15 +1,26 @@
 import './AlbumDetail.css'
 import {useState} from 'react'
+import { useDispatch, useSelector } from 'react-redux';
 
 const AlbumDetail = ({album}) => {
     const {title, id, previewImage, userId} = album;
+    const [showSongs, setShowSongs] = useState(false);
+    //TODO get songs state after we dispatch for get album songs.
+
     //TODO when you click on the element change the icon arrow to an arrow pointing down.
     //TODO when a user clicks on the album a drop down of its songs will render.
+    const handleAlbumClick = (e) =>{
+
+        setShowSongs(!showSongs);
+    }
+    console.log(showSongs);
     return(
         <>
-            <div className="album-container">
+            <div className="album-container" onClick={handleAlbumClick}>
                 <div className='chevron'>
-                    <i className="fas fa-chevron-right"></i>
+                    {showSongs ? <i class="fas fa-chevron-down"></i> : <i className="fas fa-chevron-right"></i>}
+
+
                 </div>
                 <div className='album-title'>
                     {title}
@@ -18,6 +29,23 @@ const AlbumDetail = ({album}) => {
                     Edit
                 </div>
             </div>
+            {showSongs && (
+                            <div className='songs-container'>
+                            <ul className='songs-list'>
+                                <li>
+                                    place holder song.
+                                </li>
+                                <li>
+                                    place holder song.
+                                </li>
+                                <li>
+                                    place holder song.
+                                </li>
+
+                            </ul>
+                    </div>
+                        )}
+
 
         </>
     )
