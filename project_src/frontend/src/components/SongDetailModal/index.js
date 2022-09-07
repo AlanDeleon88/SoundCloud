d62c8ea5ecba6836725a069d7ce8757da272b64a
AlanDeleon88 <19deleon88@gmail.com>
2022-09-06 20:55:26 -0700

    added some imports for song detail modal

--- project_src/frontend/src/components/SongDetailModal/index.js
+++ project_src/frontend/src/components/SongDetailModal/index.js
@@ -1,9 +1,11 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { Modal } from '../../context/Modal';
+import { useSelector } from 'react-redux';
+import { loadAlbumSongs } from '../../store/songs';
 import SongDetail from './AddAlbumForm';
 import './AddAlbumForm.css';
 
-const SongDetailModal = () => {
+const SongDetailModal = ({album}) => {
     const [showModal, setShowModal] = useState(false);
     //!might have to render the songs in here instead of on the album song list page.
     return (
