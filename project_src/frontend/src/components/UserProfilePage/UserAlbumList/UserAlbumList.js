e62b93f928e4bc3e351817efbd80dff03bb6242d
AlanDeleon88 <19deleon88@gmail.com>
2023-02-10 03:07:37 -0800

    added place holders for users with no content on their page

--- project_src/frontend/src/components/UserProfilePage/UserAlbumList/UserAlbumList.js
+++ project_src/frontend/src/components/UserProfilePage/UserAlbumList/UserAlbumList.js
@@ -2,12 +2,14 @@ import { useState, useEffect } from "react"
 import { useSelector, useDispatch } from "react-redux"
 import { loadUserAlbums } from "../../../store/albums"
 import SongListComponent from "../../SongListComponent/SongListComponent"
+import './UserAlbumList.css'
 
 
 const UserAlbumList = ({userId,username}) =>{
     const [isLoaded, setIsLoaded] = useState(false)
     const dispatch = useDispatch()
     const albums = Object.values(useSelector(state => state.albums))
+    const current_user = useSelector(state=>state.session.user)
 
     useEffect(() =>{
         dispatch(loadUserAlbums(userId)).then((res) =>{
@@ -22,17 +24,44 @@ const UserAlbumList = ({userId,username}) =>{
             <div className="album-list-main-container">
 
                     { isLoaded &&
-                        <>
-                           {
-                                albums.map((album) =>{
-                                    return(
-                                    <>
-                                        <SongListComponent album={album} username={username}/>
-                                    </>
+                            <>
+
+                                { albums.length > 0 ?
+                                    (
+                                        <>
+                                            {
+                                                albums.map((album) =>{
+                                                    return(
+                                                    <>
+                                                        <SongListComponent album={album} username={username}/>
+                                                    </>
+                                                    )
+                                                    })
+                                            }
+
+                                        </>
                                     )
-                    })
-                           }
-                        </>
+                                    :
+                                    (
+                                        <>
+                                            <div className="album-list-placeholder">
+                                                <div>
+                                                    Looks like there are no albums to be found D=
+                                                </div>
+                                            { (current_user && current_user.id === userId) &&
+                                                <div className="album-list-new-container">
+                                                    <div>
+                                                        lets fix that
+                                                    </div>
+                                                </div>
+                                            }
+                                            </div>
+                                        </>
+                                    )
+                                }
+                            </>
+
+
 
                     }
 
--- project_src/frontend/src/components/UserProfilePage/UserPlaylistList/UserPlaylistList.js
+++ project_src/frontend/src/components/UserProfilePage/UserPlaylistList/UserPlaylistList.js
@@ -2,11 +2,13 @@ import { useState, useEffect } from "react"
 import { useSelector, useDispatch } from "react-redux"
 import { loadUserPlaylists } from "../../../store/userPlaylist"
 import SongListComponent from "../../SongListComponent/SongListComponent"
+import './UserPlaylistList.css'
 
 const UserPlaylistList = ({userId, username}) =>{
     const [isLoaded, setIsLoaded] = useState(false)
     const dispatch = useDispatch()
     const playlists = Object.values(useSelector(state => state.userPlaylists))
+    const current_user = useSelector(state =>state.session.user)
 
     useEffect(() =>{
         dispatch(loadUserPlaylists(userId)).then((res) =>{
@@ -22,15 +24,41 @@ const UserPlaylistList = ({userId, username}) =>{
 
                     { isLoaded &&
                         <>
-                           {
-                                playlists.map((playlist) =>{
-                                    return(
+                            { playlists.length > 0 ?
+                                (
                                     <>
-                                        <SongListComponent playlist={playlist} username={username}/>
+                                        {
+                                                playlists.map((playlist) =>{
+                                                    return(
+                                                    <>
+                                                        <SongListComponent playlist={playlist} username={username}/>
+                                                    </>
+                                                    )
+                                                })
+                                        }
+
                                     </>
-                                    )
-                    })
-                           }
+                                )
+                                :
+                                (
+                                    <>
+                                        <div className="playlist-list-list-placeholder">
+                                            <div>
+                                                Looks like there are no playlists to be found D=
+                                            </div>
+                                        { (current_user && current_user.id === userId) &&
+                                            <div className="playlist-list-new-list-container">
+                                                <div>
+                                                    go find a song to add to a playlist!
+                                                </div>
+                                            </div>
+                                        }
+                                        </div>
+                                    </>
+                                )
+
+                            }
+
                         </>
 
                     }
